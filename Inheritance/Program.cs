2602fdbcc3a138fd20285c717897eccc6f34faad
FRANK TEI-DEKU(10954065) <ftei-deku@st.ug.edu.gh>
2024-06-18 03:40:49 +1400

    feat: add Inheritance project

---
+++ Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound(); //This method will Output animal sound: Some generic sound
+            dog.MakeSound();           // This method will Output a dog sound: Bark
+            cat.MakeSound();           // This method will Output a cat sound: Meow
+        }
+    }
+}
